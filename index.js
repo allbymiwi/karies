e18079ef1407824b8bb5bb61f5e84892c6f1dc8b
allbymiwi <allbymiri@gmail.com>
2025-11-16 23:33:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -238,12 +238,12 @@ async function runInteractorAnimation(action) {
   } else if (action === 'healthy') {
     // wortel: start high above and a bit in front (will fall)
     localStart.set(0.0, 1.6, 0.9);
-    localRot.set(-0.25, 0, 0);
+    localRot.set(-0.25, 0, 0.5);
     localScale.set(0.34,0.34,0.34); // slightly smaller
   } else if (action === 'sweet') {
     // permen: start higher & further, small
     localStart.set(0.08, 1.8, 0.95);
-    localRot.set(0, 0.4, 0.1);
+    localRot.set(0, 0.4, 0.8);
     localScale.set(0.26,0.26,0.26); // smaller
   }
 
@@ -690,6 +690,7 @@ function render(time, frame) {
 
 // initialize
 initThree();
+
 
 
 
